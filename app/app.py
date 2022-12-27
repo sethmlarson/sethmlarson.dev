a02a503e3405524a8edba0d92e207ab3075bcad8
Seth Michael Larson <sethmichaellarson@gmail.com>
2022-12-27 10:34:07 -0600

    new favicon, who dis

--- app/app.py
+++ app/app.py
@@ -169,7 +169,7 @@ def load_rss_response():
 
     feed = AtomFeed(
         title="Seth Michael Larson",
-        subtitle="Blogging about Python and the Internet",
+        subtitle="Python, open source, security, and the internet",
         author={
             "name": "Seth Michael Larson",
             "email": "sethmichaellarson@gmail.com",
