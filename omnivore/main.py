d726390bed7cf6daa71075848208543d7016709d
Seth Michael Larson <sethmichaellarson@gmail.com>
2024-05-04 18:11:56 -0500

    Add Omnivore script for generating links

---
+++ omnivore/main.py
@@ -0,0 +1,42 @@
+import os
+
+from dateutil.parser import parse as parse_datetime
+from omnivoreql import OmnivoreQL
+
+
+def main():
+    omnivore = OmnivoreQL(api_token=os.environ["OMNIVORE_API_TOKEN"])
+    labels = [node["name"][1:] for node in omnivore.get_labels()["labels"]["labels"] if node["name"].startswith("+") and node["name"] != "+RT"]
+
+    print("<p>")
+    for label in sorted(labels):
+        print(f"<h2>{label}</h2><ul>")
+
+        cursor = None
+        while True:
+            articles = omnivore.get_articles(
+                cursor=cursor,
+                query=f"in:all label:\"+{label}\" label:\"+RT\""
+            )
+            edges = articles["search"]["edges"]
+            article_date_name_url_author = []
+            for edge in edges:
+                article_date = parse_datetime(edge['node']['createdAt'])
+                article_date_name_url_author.append((
+                    article_date,
+                    edge['node']['title'],
+                    edge['node']['url'],
+                    edge['node']['author'] or None
+                ))
+            for date, title, url, author in sorted(article_date_name_url_author, reverse=True):
+                print(f"<li>“<a href=\"{url}\">{title}</a>”{' by <strong>' + author + '</strong>' if author else ''}</li>")
+            if not edges:
+                break
+            cursor = articles["search"]["edges"][-1]["cursor"]
+
+        print("</ul>")
+    print("</p>")
+
+
+if __name__ == "__main__":
+    main()
